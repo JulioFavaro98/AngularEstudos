28eca59a2a984bac11d28ee2af7068433af8396b
Julio Favaro <juliofv66@gmail.com>
2023-10-30 14:37:52 -0300

    Adiciona botões para exportar lista de CPF/CNPJ em excel e pdf

--- src/app/modules/exemplos/cpf-cnpj/cpf-cnpj.component.ts
+++ src/app/modules/exemplos/cpf-cnpj/cpf-cnpj.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import * as XLSX from 'xlsx';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
 
 @Component({
   selector: 'app-cpf-cnpj',
@@ -13,6 +16,8 @@ export class CpfCnpjComponent {
   listaDocumentos: any[] = [];
   tipoDocumento: string = 'cpf';
   documento: string = '';
+  res: boolean = false;
+  nomeArquivo: string = 'file.xlsx';
 
   documentoEmEdicao: number | null = null;
   tipoDocumentoEdit: string = 'cpf';
@@ -54,7 +59,14 @@ export class CpfCnpjComponent {
   }
 
   excluirDocumento(index: number) {
-    this.listaDocumentos.splice(index, 1);
+    this.res = confirm("Confirmar exclusão?");
+    if (this.res == true){
+      this.listaDocumentos.splice(index, 1);
+    }
+  }
+
+  ordenarPorDocs(){
+    this.listaDocumentos.sort((a, b) => a.tipo.localeCompare(b.tipo));
   }
 
   voltar(){
@@ -66,4 +78,73 @@ export class CpfCnpjComponent {
     this.documento = '';
   }
 
+  exportExcel(){
+    const table = document.getElementById("table-data");
+
+    if (table){
+      const cloneTable = table.cloneNode(true) as Element;
+
+      const acoesColumns = cloneTable.querySelectorAll('.acoes-column');
+    
+      acoesColumns.forEach((acoesColumn) => {
+      acoesColumn.remove();
+      });
+
+      const rows = cloneTable.querySelectorAll('tr');
+
+      for (let i = 1; i < rows.length; i++) {
+      const row = rows[i];
+      const cells = row.getElementsByTagName('td');
+
+
+        if (cells.length > 2) {
+          row.removeChild(cells[2]);
+        }
+      }
+
+      const ws:XLSX.WorkSheet = XLSX.utils.table_to_sheet(cloneTable);
+
+      const wb:XLSX.WorkBook = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+      XLSX.writeFile(wb, this.nomeArquivo);
+    } else {
+      console.log("Tabela não encontrada");
+    }
+    
+  }
+
+  exportPDF(){
+    const doc = new jsPDF();
+    const table = document.getElementById('table-data');
+
+    if (table) {
+      const data: any[] = [];
+      const rows = table.querySelectorAll('tr');
+
+      rows.forEach((row) => {
+        const rowData = [];
+        const cells = row.getElementsByTagName('td');
+
+        for (let i = 0; i < cells.length; i++) {
+          if (i !== 2) { // Não inclui a terceira coluna (Ações)
+            rowData.push(cells[i].innerText);
+          }
+        }
+
+        data.push(rowData);
+      });
+
+      const tableConfig = {
+        startY: 10,
+        head: [['Tipo Documento', 'Número Documento']],
+        body: data,
+      };
+
+      (doc as any).autoTable(tableConfig); 
+      doc.save('table.pdf');
+    } else {
+      console.log('Tabela não encontrada');
+    }
+  }
 }
